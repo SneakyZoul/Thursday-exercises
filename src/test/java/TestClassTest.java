e21be06c07f4b0f82c8ae9d96258bee3ea68cd74
Christian C <cph-cs450@cphbusiness.dk>
2022-02-03 07:47:09 +0100

    added the last

--- src/test/java/TestClassTest.java
+++ src/test/java/TestClassTest.java
@@ -1,9 +1,43 @@
+import DBConnector.DBConector;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestClassTest {
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
+        System.out.println("TESTINNNNGGGG");
+        Connection con = null;
+        try {
+            con = DBConector.connection();
+            String createTable = "CREATE TABLE IF NOT EXISTS `startcode_test`.`usertable` (\n" +
+                    "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
+                    "  `fname` VARCHAR(45) NULL,\n" +
+                    "  `lname` VARCHAR(45) NULL,\n" +
+                    "  `pw` VARCHAR(45) NULL,\n" +
+                    "  `phone` VARCHAR(45) NULL,\n" +
+                    "  `address` VARCHAR(45) NULL,\n" +
+                    "  PRIMARY KEY (`id`));";
+            con.prepareStatement(createTable).executeUpdate();
+            String SQL = "INSERT INTO startcode_test.usertable (fname, lname, pw, phone, address) VALUES (?,?,?,?,?)";
+            PreparedStatement ps = con.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, "Hans");
+            ps.setString(2, "Hansen");
+            ps.setString(3, "Hemmelig123");
+            ps.setString(4, "40404040");
+            ps.setString(5,"Rolighedsvej 3");
+            ps.executeUpdate();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @org.junit.jupiter.api.AfterEach
